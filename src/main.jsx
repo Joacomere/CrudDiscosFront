a0127dd4801366e17b8885818e8ff3a1d17e5728
Programacion8 <darioramos8919@gmail.com>
2024-09-09 11:43:28 -0300

    añadimos un inicio

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,46 @@
+//Modulos
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+//componentes
 import App from './App.jsx'
 import Header from './componentes/Header.jsx'
 import Footer from './componentes/Footer.jsx'
+import Home from './componentes/Home.jsx'
+import Info from './componentes/Info.jsx'
+import Proximamente from './componentes/Proximamente.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home />,
+    errorElement: (<div>Error, esto está mal</div>),
+    
+    
+  },
+  {
+    path: "/discos",
+    element: <App/>,
+    errorElement: <div>disco no encontrado</div>
+  },
+  {
+    path:"/Info",
+    element:<Info />,
+    errorElement: (<div>Error, esto está mal</div>),
+  },  
+  {
+    path:"/Proximamente",
+      element:<Proximamente />,
+      errorElement: (<div>Error, esto está mal</div>),
+  }  
+]);
 
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header></Header>
-    <App />
-    <Footer></Footer>
+    <Header />
+    <RouterProvider router={router}/>
+    <Footer />
   </StrictMode>,
 )
